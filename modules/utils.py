87d3f45bcac40f32497292fd4b14295bc4aeae66
Ivan Nathanael <ivannathanael.inbox@gmail.com>
2024-11-19 22:11:44 +0800

    AKU SAYANG DOMINIC TORETTO <3

--- modules/utils.py
+++ modules/utils.py
@@ -1,4 +1,5 @@
 print("Hellow WORLD AKU PLORENS!!!!!!!!!!!!!!!")
+print("MONALISA AND DOMINIC")
 
 def get_curr_loc():
     # return the current location of the computer in coordinate (latitude, longitude)
