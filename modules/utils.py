a8c73728a2c41e6b1a13e27335afb6676eb3375b
flo <renny1612@icloud.com>
2024-11-27 16:42:34 +0700

    mueheh

--- modules/utils.py
+++ modules/utils.py
@@ -1,24 +1,13 @@
-import requests
-from typing import Tuple
+import geocoder
+from typing import List, Tuple
 from location import Location
 
 def get_curr_loc() -> Tuple[float, float]:
-    try:
-        # gets the public IP address
-        ip_response = requests.get('https://api.ipify.org?format=json')
-        ip_address = ip_response.json()['ip']
-
-        # use IP geolocation service to get location information based on IP
-        geo_response = requests.get(f'https://ipapi.co/{ip_address}/json/')
-        geo_data = geo_response.json()
-
-        if 'latitude' in geo_data and 'longitude' in geo_data:
-            return (geo_data['latitude'], geo_data['longitude'])  
-        else:
-            return None  
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        return None 
+    g = geocoder.ip('me')  # get location based on IP address
+    if g.ok:
+        return (g.latlng[0], g.latlng[1]) 
+    else:
+        return (None, None)  
 
 if __name__ == "__main__":
     main()
